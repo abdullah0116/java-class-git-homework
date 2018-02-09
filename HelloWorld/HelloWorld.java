d6b3246b0c2494883485d951fa78ef3041705333
abdullahkareem <akasak56@gmail.com>
2018-02-08 21:16:32 -0700

    added notes for the first video notes

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class HelloWorld {
-  public void HelloWorldGreeting() {
-    System.out.println("Hello World!");
+  public static void main (String[] args){
+    //instances
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Welcome, What is your name");
+    String name = sc.nextLine();
+     System.out.println("Hello" + name + ", Thats for the input");
   }
 }
+
+// public class HelloWorld {
+//   public void HelloWorldGreeting() {
+//     System.out.println("Hello World!");
+//   }
+// }
