e75d991629f977335739d337ecc279240d5c8e50
abdullahkareem <akasak56@gmail.com>
2018-02-16 17:45:04 -0700

    ball bouncing

--- Module 11 /hoops/engines/CollisionEngine.java
+++ Module 11 /hoops/engines/CollisionEngine.java
@@ -1,10 +1,11 @@
 package engines;
 import models.GameModel;
-
 public class CollisionEngine {
     public void updateState(GameModel state) {
         if (state.getBall().getY() < 0) {
           state.getBall().setY(0);
+          double oldYVelocity = state.getBall().getyVelocity();
+          state.getBall().setY(oldYVelocity * -.5);
         }
     }
 }
